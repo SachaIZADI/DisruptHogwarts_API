35c84ed7dc8dd428268d304b978ebe9f4a7ced54
SachaIZADI <sacha.izadi@polytechnique.edu>
2018-10-10 14:23:06 +0200

    starting to write the api

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from model.logistic_regression import LogisticRegression
+import request
+import numpy as np
 
 """
 - https://www.wintellect.com/creating-machine-learning-web-api-flask/
@@ -9,19 +12,50 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/predict')
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+
+            school = data['school']
+            astronomy = float(data['astronomy'])
+            herbology = float(data['herbology'])
+            ancient_runes = float(data['ancient_runes'])
+
+            # TODO : here
+            if school == 'hogwarts':
+                logreg = LogisticRegression(path_to_beta='model/results/beta.json')
+            else:
+                logreg = LogisticRegression(path_to_beta='model/results/beta_%s.json' % school)
+
+            prediction = logreg.predict(X_to_predict=np.array([[astronomy],
+                                                               [herbology],
+                                                               [ancient_runes]]))
+
+            
+            # return jsonify ({'house':house, 'proba':proba})
+
+
+
+
+
+
+
+
+
+
+
+            lin_reg = joblib.load("./linear_regression_model.pkl")
+        except ValueError:
+            return jsonify("Please enter a number.")
+
+        return jsonify(lin_reg.predict(years_of_experience).tolist())
 
 
 @app.route('/train')
 def hello_world1():
     return 'Hello World 1!'
-
-
-@app.route('/predict')
-def hello_world2():
-    return 'Hello World 2!'
 
 
 
