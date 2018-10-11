b11288aaa4c5d25c3c7423883bbf605be0d3c992
SachaIZADI <sacha.izadi@polytechnique.edu>
2018-10-11 13:46:25 +0200

    tested and debugged 90% of code

--- test_and_debug/test_api.py
+++ test_and_debug/test_api.py
@@ -1,11 +1,12 @@
 import requests
 import json
+import numpy as np
 
 
 BASE_URL = 'http://127.0.0.1:5000'
 
 
-
+"""
 # Train the model on the base dataset
 response = requests.get("{}/train".format(BASE_URL))
 print(response.text)
@@ -20,7 +21,7 @@ data = json.dumps({'school':'hogwarts',
 response = requests.get("{}/predict".format(BASE_URL), json=data)
 print(response.text)
 
-
+"""
 
 # Transfer learning
 data = json.dumps({
@@ -34,6 +35,10 @@ data = json.dumps({
         'alpha':0.01,
         'n':5,
         'batch_size':14
-    }
+    },
+    'X':np.array([[1,2,2],[1,3,1],[1,5,6],[1,4,7]]).tolist(),
+    'y':np.array(["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]).tolist()
+})
 
-                  })
+response = requests.get("{}/transfer".format(BASE_URL), json=data)
+print(response.text)
